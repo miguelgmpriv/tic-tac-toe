002f522976d3fe930130b519d953be2de3853a3e
Miguel <miguelgm.priv@gmail.com>
2021-09-08 17:43:13 -0700

    Add clearBoard function to gameBoard module

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const gameBoard = (()=>{
-    const board = document.getElementById('game-board');
+    const createBoard = () =>{
+    let board = document.getElementById('game-board');
     for (let i=0; i < 9;i++){
         let newDiv = document.createElement('div');
         newDiv.classList.add('cell');
+        newDiv.dataset.cell = i;
+        newDiv.textContent = 'X';
         board.appendChild(newDiv);   
+    }};
+    const clearBoard = () =>{
+        document.querySelectorAll('[data-cell]').forEach(cell => {
+            cell.textContent = '';
+        });
     }
+    return {createBoard, clearBoard};
 })();
+
+gameBoard.createBoard();
+gameBoard.clearBoard();
