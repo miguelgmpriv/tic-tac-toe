ebd6241174a4a04974d9aed4de414e7a7092e7c9
Miguel <miguelgm.priv@gmail.com>
2021-09-09 02:39:56 -0700

    Add boardCells and boardArray as return values of gameBoard The idea is to make the program more dynamic by using an array made when the gameBoard function is created instead of assuming the array is going to be a certain way

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 const gameBoard = (()=>{
     const board = document.getElementById('game-board');
-    const createBoard = () =>{
+    const cell = 'cell';
     for (let i=0; i < 9;i++){
         let newDiv = document.createElement('div');
-        newDiv.classList.add('cell');
-        newDiv.dataset.cell = i;
-        newDiv.textContent = 'X';
+        newDiv.classList.add(cell);
+        newDiv.dataset[cell] = i;
         board.appendChild(newDiv);   
-    }};
+    };
+    const boardCells = document.querySelectorAll(`[data-${cell}]`)
+    const boardArray = Array.from(boardCells);
+    console.log(boardArray);
     const clearBoard = () =>{
-        document.querySelectorAll('[data-cell]').forEach(cell => {
+        boardArray.forEach(cell => {
             cell.textContent = '';
         });
     }
     document.getElementById('game-restart').addEventListener('click', clearBoard);
-    return {createBoard, clearBoard};
+    return {boardCells, boardArray, clearBoard};
 })();
-gameBoard.createBoard();
 
-const playGame = (()=>{
-    document.getElementById('game-board').addEventListener('click', (e)=>{
-        e.target.textContent = 'O';
-    });
-    
+const playGame = ((condition)=>{
+
+    if (condition === true) return;
+    const board = document.getElementById('game-board');
+    board.addEventListener('click', (e)=>{
+        if (e.target.textContent !== '') return (false);
+        e.target.textContent = '0';
+        console.log('works');
+    });    
 })();
 
