54c730dea2d4da2214faf9c10fe495058cd296d4
Miguel <miguelgm.priv@gmail.com>
2021-09-10 16:26:47 -0700

    Add markBoard function Function marks the board based on playerTurn boolean. Added event listener for gameboard on playGame module.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const player = (name, mark)=>{
     const ai = false;
     const win = false;
-    return {name, mark,ai,win}
+    return {name, mark, ai, win}
 };
 
 const options = (()=>{
@@ -16,31 +16,26 @@ const options = (()=>{
     });        
     gamePlayers.addEventListener('click', ()=>{
         playerOne.name = prompt('Enter player one\'s name', 'John Doe');
-        playerTwo.name = prompt('Enter player two\'s name','Jane Doe');    
+        playerTwo.name = prompt('Enter player two\'s name','Jane Doe');
     });
     gameClear.addEventListener('click', ()=>{
         gameBoard.clearBoard();
     });
-    let players = [playerOne,playerTwo];
-    return {players}
+    return {playerOne, playerTwo}
 })();
-
-
 
 const gameBoard = (()=>{
     const board = document.getElementById('game-board');
     const cell = 'cell'; //Use class name as name for dataset
-
-    for (let i=0; i < 9;i++){
+    for (let i = 0; i < 9;i++){
         let newDiv = document.createElement('div');
         newDiv.classList.add(cell);
         newDiv.dataset[cell] = i;
-        newDiv.textContent = 'X';
         board.appendChild(newDiv);   
     };
     const boardCells = document.querySelectorAll(`[data-${cell}]`)
     const boardArray = Array.from(boardCells);
-    const clearBoard = () =>{
+    const clearBoard = ()=>{
         boardArray.forEach(cell => {
             cell.textContent = '';
         });
@@ -48,18 +43,25 @@ const gameBoard = (()=>{
     return {board, boardCells, boardArray, clearBoard};
 })();
 
-
 const checkWin = (()=>{
 
 })();
 
-/* const playGame = ((condition)=>{
-    if (condition === true) return;
+const playGame = (()=>{
+    let playerTurn = true; //player one start
     const board = gameBoard.board;
-    board.addEventListener('click', (e)=>{
-        if (e.target.textContent !== '') return (false);
-        e.target.textContent = '0';
-        console.log('works');
-    });    
-})(); */
+    const markBoard = (event)=>{
+        if (event.textContent === '') return;
+        
+        if (playerTurn){
+            console.log(options.playerOne.mark);
+            event.target.textContent = options.playerOne.mark;
+            playerTurn = false;
+        } else {
+            event.target.textContent = options.playerTwo.mark;
+            playerTurn = true;
+        }
+    };  
+    board.addEventListener('click', markBoard);
+})();
 
