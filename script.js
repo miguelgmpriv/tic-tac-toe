c066f64273bc015f7c8bb3bc54da87e6e8919558
Miguel <miguelgm.priv@gmail.com>
2021-09-11 03:36:05 -0700

    Add matrix function to gameWin module matrix changes the array to a multi dimensional array. Going to attempt to check for win using true/false check in nested for loops going through the newly made testArray.

--- script.js
+++ script.js
@@ -26,26 +26,24 @@ const options = (()=>{
 
 const gameBoard = (()=>{
     const board = document.getElementById('game-board');
+    const gridSize = 3;
     const cell = 'cell'; //Use class name as name for dataset
-    for (let i = 0; i < 9;i++){
+    for (let i = 0; i < (gridSize*gridSize);i++){
         let newDiv = document.createElement('div');
         newDiv.classList.add(cell);
         newDiv.dataset[cell] = i;
         board.appendChild(newDiv);   
     };
-    const boardCells = document.querySelectorAll(`[data-${cell}]`)
-    const boardArray = Array.from(boardCells);
+    const boardArray = Array.from(document.querySelectorAll(`[data-${cell}]`));
     const clearBoard = ()=>{
         boardArray.forEach(cell => {
             cell.textContent = '';
         });
     }
-    return {board, boardCells, boardArray, clearBoard};
+    return {board, boardArray, gridSize, clearBoard};
 })();
 
-const checkWin = (()=>{
 
-})();
 
 const playGame = (()=>{
     let playerTurn = true; //player one start
@@ -63,7 +61,38 @@ const playGame = (()=>{
             markCell.textContent = playerTwo.mark;
             playerTurn = true;
         }
+        return gameWin.checkWin()
     };  
     board.addEventListener('click', markBoard);
 })();
 
+const gameWin = (()=>{
+    const matrix = (arr, size) =>
+        arr.reduce((rows, key, index) => (index % size == 0 ? rows.push([key]) 
+            : rows[rows.length-1].push(key)) && rows, []); 
+    const checkWin = () =>{
+        const testArray = matrix(gameBoard.boardArray, gameBoard.gridSize);
+        console.log(testArray);
+        let win = false;
+        for (let i = 0; i < gameBoard.gridSize; i++){
+            let won = true;
+            for (let x = 0; x < (gameBoard.gridSize - 1); x++){
+                if (row[i][x].textContent === '' || won === false){
+                    won = false;
+                    break;
+                }
+                if (row[i][x].textContent !== row[i][x+1].textContent){
+                    won = false;
+                }
+            }
+            if (won === true){
+                win = true;
+                break;
+            }
+        }
+        if (win === true){
+            console.log('you won');
+        }
+    }
+    return {checkWin}
+})();
